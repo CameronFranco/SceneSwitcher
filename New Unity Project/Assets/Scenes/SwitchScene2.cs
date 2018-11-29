b06f6b1a3e3f77c7a9671080351c1b319c11d1b3
CameronFranco <44078559+CameronFranco@users.noreply.github.com>
2018-11-29 15:51:31 -0500

    SceneSwitcher completed

---
+++ New Unity Project/Assets/Scenes/SwitchScene2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene2 : MonoBehaviour
+{
+
+    public void SwitchScenes()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
